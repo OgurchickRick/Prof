c221a8f5a795e7a455602688129c089e44811651
артем дербин <artem.derbinkpc612020@gmail.com>
2023-10-26 09:58:25 +0300

    Заготовка 0.3

--- src/main/java/service/Driver.java
+++ src/main/java/service/Driver.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Configuration;
 public class Driver {
 
     public void initDriver() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         Configuration.timeout = 10000;
         Configuration.browserSize = "1920x1080";
         Configuration.headless = false;
